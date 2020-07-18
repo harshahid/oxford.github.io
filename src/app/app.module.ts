26b53610d25558911d08c5dc958d1d8504b93a35
Harsh Halagalimath (BLR GSS) <hharsh@nousinfo.com>
2020-07-18 19:51:13 +0530

    Added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component'
+
+const appRoutes : Routes = [
+  {path : '', component: CarouselComponent},
+  {path : 'contact', component: ContactComponent},
+  {path : 'about', component: AboutComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    CarouselComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
